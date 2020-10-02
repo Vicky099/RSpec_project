8b83aa5258f56937b59e50b7c7530256c8f600f8
Vikram Panmand <vikky.panmand@gmail.com>
2020-10-02 16:46:17 +0530

    modified test case based on jbuilder response

--- spec/request/api/v1/products_controller_spec.rb
+++ spec/request/api/v1/products_controller_spec.rb
@@ -11,17 +11,16 @@ RSpec.describe "ProductsControllerTest", type: :request do
 	let(:product_id) { products.first.id }
 
 	
-	describe "GET /products" do
-		before { get '/products.json' }
+	describe "GET /api/v1/products" do
+		before { get '/api/v1/products', as: :json }
 
-		it 'returns articles' do
-      		# Note `json` is a custom helper to parse JSON responses
-      		expect(json).not_to be_empty
-      		expect(json.size).to eq(10)
-    	end
+		it 'returns products' do
+			expect(json).not_to be_empty
+			expect(json['data'].size).to eq(10)
+		end
 
-    	it 'returns status code 200' do
-      		expect(response).to have_http_status(200)
-    	end
+		it 'returns status code 200' do
+			expect(response).to have_http_status(200)
+		end
 	end
 end
